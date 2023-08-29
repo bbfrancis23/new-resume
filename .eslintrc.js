d4d639340b8928e17a9ddcd02c1b8d092dac7d6a
bbfrancis23 <bbfrancis23@gmail.com>
2023-08-28 19:16:14 -0700

    remove phone

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,10 +3,7 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-  ],
+  extends: ['plugin:react/recommended', 'airbnb'],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
@@ -14,14 +11,18 @@ module.exports = {
     ecmaVersion: 12,
     sourceType: 'module',
   },
-  plugins: [
-    'react',
-  ],
+  plugins: ['react'],
   rules: {
     semi: ['error', 'never'],
-    'max-len': ['error', { code: 125 }],
-    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+    'max-len': ['error', {code: 125}],
+    'import/no-extraneous-dependencies': ['error', {devDependencies: true}],
     'react/jsx-props-no-spreading': 'off',
-    'max-lines': ['error', { max: 100, skipBlankLines: true, skipComments: true }],
+    'max-lines': ['error', {max: 100, skipBlankLines: true, skipComments: true}],
+    'linebreak-style': 'off',
+    'object-curly-spacing': 'off',
+    'object-curly-newline': 'off',
+    indent: 'off',
+    'react/jsx-closing-bracket-location': 'off',
+    'no-console': 'off',
   },
 }
